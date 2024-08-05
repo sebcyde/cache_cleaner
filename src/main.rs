4c22c66ad6901833990f1da96fda2b897dc306d2
Sebastian Cyde <sebastian.cyde@red-engine.com>
2024-08-05 17:00:32 +0100

    Complete - Staging V

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,12 @@ fn main() {
     println!("\nRunning Cache Cleaner!\n");
 
     let es_handle = thread::spawn(move || connect_and_clean(COMPANY::ELECTRICSHUFFLE));
-    // let fc_handle = thread::spawn(move || connect_and_clean(COMPANY::FLIGHTCLUB));
-    // let re_handle = thread::spawn(move || connect_and_clean(COMPANY::REDENGINE));
+    let fc_handle = thread::spawn(move || connect_and_clean(COMPANY::FLIGHTCLUB));
+    let re_handle = thread::spawn(move || connect_and_clean(COMPANY::REDENGINE));
 
     es_handle.join().unwrap();
-    // fc_handle.join().unwrap();
-    // re_handle.join().unwrap();
+    fc_handle.join().unwrap();
+    re_handle.join().unwrap();
 
     println!("\nFinished clearing caches.\n");
 }
--- src/server.rs
+++ src/server.rs
@@ -11,9 +11,9 @@ pub mod server {
     use crate::COMPANY;
 
     pub fn connect_and_clean(company: COMPANY) {
-        println!("\nConnecting to server...");
         dotenv().ok();
 
+        let mut us_dir_path: String = String::new();
         let hostname: String;
         let username: String;
         let password: String;
@@ -21,13 +21,16 @@ pub mod server {
 
         match company {
             COMPANY::ELECTRICSHUFFLE => {
+                println!("\nConnecting to ES server...");
                 println!("Cleaning ES...");
                 hostname = env::var("ES_HOSTNAME").expect("ES hostname not found");
                 username = env::var("ES_USERNAME").expect("ES username not found");
                 password = env::var("ES_PASSWORD").expect("ES password not found");
-                dir_path = env::var("ES_DIR_PATH").expect("ES dirPath not found");
+                dir_path = env::var("ES_UK_DIR_PATH").expect("ES UK dirPath not found");
+                us_dir_path = env::var("ES_US_DIR_PATH").expect("ES US dirPath not found");
             }
             COMPANY::FLIGHTCLUB => {
+                println!("\nConnecting to FC server...");
                 println!("Cleaning FC...");
                 hostname = env::var("FC_HOSTNAME").expect("FC hostname not found");
                 username = env::var("FC_USERNAME").expect("FC username not found");
@@ -35,6 +38,7 @@ pub mod server {
                 dir_path = env::var("FC_DIR_PATH").expect("FC dirPath not found");
             }
             COMPANY::REDENGINE => {
+                println!("\nConnecting to RE server...");
                 println!("Cleaning RE...");
                 hostname = env::var("RE_HOSTNAME").expect("RE hostname not found");
                 username = env::var("RE_USERNAME").expect("RE username not found");
@@ -60,12 +64,36 @@ pub mod server {
 
         let mut output = String::new();
         channel.read_to_string(&mut output).unwrap();
-        println!("Command output: {}", output);
 
         let mut stderr = String::new();
         channel.stderr().read_to_string(&mut stderr).unwrap();
         if !stderr.is_empty() {
             eprintln!("Command error: {}", stderr);
+        }
+
+        if !us_dir_path.is_empty() {
+            // US
+
+            let mut us_channel: Channel = sess.channel_session().unwrap();
+
+            let us_delete_command: String = format!("rm -rf {}", us_dir_path);
+            us_channel
+                .exec(&us_delete_command)
+                .expect("Failed to execute delete command");
+
+            let mut us_output = String::new();
+            us_channel.read_to_string(&mut us_output).unwrap();
+
+            let mut us_stderr = String::new();
+            us_channel.stderr().read_to_string(&mut us_stderr).unwrap();
+            if !stderr.is_empty() {
+                eprintln!("US Command error: {}", us_stderr);
+            }
+
+            us_channel
+                .send_eof()
+                .expect("Failure in US channel send_eof");
+            us_channel.wait_close().expect("Error closing US channel");
         }
 
         channel.send_eof().expect("Failure in channel send_eof");
